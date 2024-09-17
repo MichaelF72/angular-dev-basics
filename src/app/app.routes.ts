8aa1d3e27f5f696e6a9bae3d93efc6235cfaf980
Michael Feiler <michael.feiler@gmx.de>
2024-09-17 22:05:21 +0200

    app-05-routing implementiert

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './05-routing/about/about.component';
-import { ItemDetailComponent } from './05-routing/item-detail/item-detail.component';
-import { RoutingComponent } from './05-routing/routing.component';
+import { HomeComponent } from './05-routing/home/home.component';
 
 // (1) Der Router verwaltet die URLs der Anwendung.
 // (2) Die Routen legen fest, welche Komponente für welche URL geladen wird.
 export const routes: Routes = [
-  { path: '', component: RoutingComponent }, // (3) Pfade in Routen ohne führenden Slash.
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'item/:id', component: ItemDetailComponent }, // (9) Übergabe von Routenparametern.
-  { path: '**', redirectTo: '' }, // (14) Wildcard-Route zur Umleitung auf die Startseite.
 ];
