709ee32f4db2687c1a8fe7afea270df809f0c037
Michael Feiler <michael.feiler@gmx.de>
2024-10-19 16:25:53 +0200

    Restliche Apps in AppComponent eingetragen.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,15 @@ import { DependencyInjectionComponent } from './04-dependency-injection/dependen
 import { RoutingComponent } from './05-routing/routing.component';
 import { HttpComponent } from './06-http/http.component';
 import { RxjsExampleComponent } from './07-rxjs/rxjs.component';
-import { InterceptorsComponent } from './08-interceptors/interceptors.component'; // Import der neuen Komponente
+import { InterceptorsComponent } from './08-interceptors/interceptors.component';
+import { TemplateDrivenFormsComponent } from './09-template-driven-forms/template-driven-forms.component';
+import { ReactiveFormsComponent } from './10-reactive-forms/reactive-forms.component';
+import { FormularvalidierungComponent } from './11-formularvalidierung/formularvalidierung.component';
+import { PipesComponent } from './12-pipes/pipes.component';
+import { DirektivenComponent } from './13-direktiven/direktiven.component';
+import { LazyLoadingComponent } from './14-lazy-loading/lazy-loading.component';
+import { GuardsComponent } from './15-guards/guards.component';
+import { StandaloneComponentsComponent } from './16-standalone-components/standalone-components.component'; // Import der neuen Komponente
 
 @Component({
   selector: 'app-root',
@@ -22,6 +30,14 @@ import { InterceptorsComponent } from './08-interceptors/interceptors.component'
     HttpComponent,
     RxjsExampleComponent,
     InterceptorsComponent,
+    TemplateDrivenFormsComponent,
+    ReactiveFormsComponent,
+    FormularvalidierungComponent,
+    PipesComponent,
+    DirektivenComponent,
+    LazyLoadingComponent,
+    GuardsComponent,
+    StandaloneComponentsComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
