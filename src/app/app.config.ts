d571015bfded22091a3617e5841c0277e5aeef94
Michael Feiler <michael.feiler@rewe-group.com>
2024-11-08 15:02:58 +0100

    Neuer Einbau 14-lazy-loading.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; // Importiere die Routen
@@ -8,7 +8,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     // Routen werden hier eingebunden
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withPreloading(PreloadAllModules), // Preloading-Strategie hinzugefügt
+    ),
     provideHttpClient(), // HTTP-Client Provider hinzufügen
     // (5) Registration des Interceptors mit dem neuen HttpClient-System für Standalone Components.
     provideHttpClient(withInterceptorsFromDi()),
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,4 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 // Die Anwendung wird mit der Standalone Component ohne NgModule gebootstrapped.
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
